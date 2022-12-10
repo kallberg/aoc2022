ce13d4243a47a91ca719801f843442406804dd29
Erik Källberg <erik@kallberg.dev>
2022-12-10 10:57:25 +0100

    day10 pt 1

---
+++ src/day10pt1.rs
@@ -0,0 +1,94 @@
+#[derive(Debug, Clone)]
+pub enum Instruction {
+    Noop,
+    Addx(i64),
+}
+
+impl Instruction {
+    pub fn execute(&self, cpu: &mut Cpu) -> bool {
+        match self {
+            Instruction::Noop => true,
+            Instruction::Addx(x) => {
+                if cpu.instruction_cycle == 1 {
+                    cpu.register_x += x;
+                    true
+                } else {
+                    cpu.instruction_cycle += 1;
+                    false
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Cpu {
+    pub register_x: i64,
+    pub program: Vec<Instruction>,
+    pub program_counter: usize,
+    pub instruction: Option<Instruction>,
+    pub instruction_cycle: usize,
+    pub cycle: i64,
+}
+
+impl Cpu {
+    pub fn tick(&mut self) -> bool {
+        if self.instruction.is_none() {
+            if self.program_counter >= self.program.len() - 1 {
+                return false;
+            }
+
+            self.instruction = Some(self.program[self.program_counter].clone());
+            self.program_counter += 1;
+            self.instruction_cycle = 0;
+        }
+
+        let instruction = self.instruction.clone().unwrap();
+
+        if instruction.execute(self) {
+            self.instruction = None;
+        }
+
+        self.cycle += 1;
+        true
+    }
+}
+
+impl From<&str> for Instruction {
+    fn from(line: &str) -> Self {
+        if line.starts_with("noop") {
+            return Self::Noop;
+        }
+
+        if line.starts_with("addx") {
+            let x_str = line.strip_prefix("addx ").unwrap();
+            let x: i64 = x_str.parse().unwrap();
+            return Self::Addx(x);
+        }
+
+        unreachable!()
+    }
+}
+
+pub fn solve(input: &str) -> i64 {
+    let mut cpu = Cpu {
+        cycle: 1,
+        register_x: 1,
+        program: input.lines().map(Instruction::from).collect(),
+        ..Default::default()
+    };
+
+    let mut signal_strengths = vec![];
+
+    while cpu.tick() {
+        match cpu.cycle {
+            20 | 60 | 100 | 140 | 180 | 220 => {
+                // println!("register_x={}, cycle={}", cpu.register_x, cpu.cycle);
+                signal_strengths.push(cpu.cycle * cpu.register_x)
+            }
+            _ => {}
+        }
+    }
+
+    signal_strengths.iter().sum()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod day10pt1;
+mod day10pt2;
 mod day1pt1;
 mod day1pt2;
 mod day2pt1;
@@ -38,13 +40,15 @@ fn main() {
     println!("{}", day8pt2::solve(include_str!("../input/day8pt1.txt")));
     println!("{}", day9pt1::solve(include_str!("../input/day9pt1.txt")));
     println!("{}", day9pt2::solve(include_str!("../input/day9pt1.txt")));
+    println!("{}", day10pt1::solve(include_str!("../input/day10pt1.txt")));
+    println!("{}", day10pt2::solve(include_str!("../input/day10pt1.txt")));
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{
-        day1pt1, day1pt2, day2pt1, day2pt2, day3pt1, day3pt2, day4pt1, day4pt2, day5pt1, day5pt2,
-        day6pt1, day6pt2, day7pt1,
+        day10pt1, day1pt1, day1pt2, day2pt1, day2pt2, day3pt1, day3pt2, day4pt1, day4pt2, day5pt1,
+        day5pt2, day6pt1, day6pt2, day7pt1,
         day7pt1::Directory,
         day7pt2, day8pt1,
         day8pt1::TreeGrid,
@@ -173,5 +177,19 @@ mod tests {
         let input = include_str!("../example_input/day9pt2.txt");
 
         assert_eq!(day9pt2::solve(input), 36);
+    }
+
+    #[test]
+    fn day10pt1_eq_example() {
+        let input = include_str!("../example_input/day10pt1.txt");
+
+        assert_eq!(day10pt1::solve(input), 13140);
+    }
+
+    #[test]
+    fn day10pt2_eq_example() {
+        let input = include_str!("../example_input/day10pt1.txt");
+
+        assert_eq!(day10pt1::solve(input), 13140);
     }
 }
